c21d3213cd9ef2908c2e79c06f55894e58571b35
jeroenvanbaar <jvb@brown.edu>
2019-08-11 17:26:09 -0400

    started function base

--- code/functions.py
+++ code/functions.py
@@ -1,4 +1,4 @@
-import os, re, nltk, html2text, markdown
+import os, re, nltk, html2text, markdown, requests
 import numpy as np
 import pandas as pd
 import brainiak.eventseg.event as event
@@ -6,6 +6,37 @@ import hypertools.tools.format_data as fit_transform
 from num2words import num2words
 from bs4 import BeautifulSoup
 from downloader import download_file_from_google_drive as dl
+
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
 
 def load_data(filepath, fileid):
     data_dir = os.path.dirname(filepath)
